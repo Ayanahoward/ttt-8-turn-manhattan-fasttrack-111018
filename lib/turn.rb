c997b53f5dfc24235ef870a8a6c6b2ca1c61df0c
trustworthy-edition-4386 <g6cleo@hotmail.com>
2018-11-11 19:48:35 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+#turn
+def display_baord (board)
+board = ["o","o","x","x","x","o","x","o","x"]
+puts "#{board [0]}"| "#{board [1]}" |"#{board[2]}"
+puts "----------"
+puts "#{board [3]}"| "#{board [4]}" |"#{board[5]}"
+puts "----------"
+puts "#{board [6]}"| "#{board [7]}" |"#{board[8]}"
+
+end 
+
+
+
+
+
+
+
+  
+    
